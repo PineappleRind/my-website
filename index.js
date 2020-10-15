f042aa2b7fbfb8ccdd9921c8c499da312b832999
PineappleRind <pineapplerind.info@gmail.com>
2020-10-14 21:26:43 -0400

    dark design

---
+++ index.js
@@ -0,0 +1,6 @@
+window.addEventListener("keydown", function (e) {
+    //tested in IE/Chrome/Firefox
+    if (e.key === 't' || e.key === 'y') {
+      window.location.href = "https://www.youtube.com/watch?v=oHg5SJYRHA0";
+    }
+  })
