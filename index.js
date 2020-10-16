e9258646f63b05b9cf25c89ec49bc39c61f88b2a
PineappleRind <pineapplerind.info@gmail.com>
2020-10-15 20:32:17 -0400

    buttons work now

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 window.addEventListener("keydown", function (e) {
-    //tested in IE/Chrome/Firefox
-    if (e.key === 't' || e.key === 'y') {
+    if (e.key === 'y') {
       window.location.href = "https://www.youtube.com/watch?v=oHg5SJYRHA0";
     }
   })
+
+  function snackbar() {
+    var x = document.getElementById("snackbar");
+    x.className = "appear";
+    let y = document.getElementById("overlay");
+    y.style.display = 'block';
+    setTimeout(function() {y.style.opacity = '1';},1)
+    setTimeout(function(){ x.className = x.className.replace("appear", ""); y.style.opacity = '0'; }, 3000);
+    setTimeout(function() {
+      y.style.display = 'none';
+    },4000)
+  }
